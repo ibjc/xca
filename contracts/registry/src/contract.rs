2aeb5df796326f83d4c73c5c3c9108f727b90bf5
Ubuntu <ubuntu@ip-172-31-20-57.ec2.internal>
2022-10-20 20:00:43 +0000

    query/instantiate impld

--- contracts/registry/src/contract.rs
+++ contracts/registry/src/contract.rs
@@ -1,10 +1,10 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, to_binary};
 // use cw2::set_contract_version;
 
-use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
+use xca::registry::{ExecuteMsg, InstantiateMsg, QueryMsg, ConfigResponse};
 
 /*
 // version info for migration info
@@ -14,27 +14,55 @@ const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
-) -> Result<Response, ContractError> {
-    unimplemented!()
+    msg: InstantiateMsg,
+) -> StdResult<Response>  {
+
+    CONFIG.save(deps.storage, &Config{
+        wormhole_core_contract: deps.api.addr_validate(&msg.wormhole_core_contract)?,
+        x_account_factory: deps.api.addr_validate(&msg.x_account_factory)?,
+        wormhole_chain_ids: msg.wormhole_chain_ids,
+        x_account_code_id: msg.x_account_code_id,
+    })?;
+
+    Ok(Response::new())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
-    unimplemented!()
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> StdResult<Response>  {
+    match msg {
+        ExecuteMsg::UpdateConfig{wormhole_core_contract, x_account_factory, x_account_code_id} => {
+            Ok(Response::new())
+        },
+        ExecuteMsg::UpsertWormholeChainId{chain} => {
+
+            Ok(Response::new())
+        },
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    unimplemented!()
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg{
+        QueryMsg::Config {} => {
+            let config: Config = CONFIG.load(deps.storage)?;
+    
+            Ok(to_binary(&ConfigResponse{
+                wormhole_core_contract: config.wormhole_core_contract.to_string(),
+                x_account_factory: config.x_account_factory.to_string(),
+                wormhole_chain_ids: config.wormhole_chain_ids,
+                x_account_code_id: config.x_account_code_id,
+            })?)
+        }
+    }
+
 }
 
 #[cfg(test)]
